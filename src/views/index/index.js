e4c643bd8a6c4b23d10544f5bb75bf1974ee2b83
hys81021 <hys81021@163.com>
2020-10-18 00:21:27 +0800

    界面布局

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,13 +1,27 @@
 // 首页
 import React, { Component } from "react";
+// layout组件
+import Aside from "./components/aside";
+// 样式引入
+import "./layout.scss";
+// antd引入
+import { Layout } from "antd";
+const { Sider, Header, Content } = Layout;
+
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
-    render(){
+    render() {
         return (
-            <div>我是首页</div>
+            <Layout className="layout-wrap">
+                <Sider width="250px"><Aside/></Sider>
+                <Layout>
+                    <Header className="layout-header">头部</Header>
+                    <Content className="layout-main">内容</Content>
+                </Layout>
+            </Layout>
         )
     }
 }
